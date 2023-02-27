2a1c69a50f28c56a3ea45cd0a8080b8ab1172b7c
Xue Yifan <xueyifan@mail.sdu.edu.cn>
2023-02-27 15:14:07 +0800

    Delete Surface Vehicle/BlackBox directory

--- Surface Vehicle/BlackBox/Blackmodel_ac/jointDistribution.m
+++
@@ -1,9 +0,0 @@
-%  m,u are vectors
-%add control inputs to the GP inputs
-function [tileM, tileS] = jointDistribution(m, s, u)
-    dimS = size(s, 1);
-    dimU = size(u,1);
-    tileM = [m; u];
-    tileS = zeros(dimS+dimU,dimS+dimU);
-    tileS(1:dimS, 1:dimS) = s;
-end
