afd940142790dfea3c7bae1fc3ad9394d55ddb52
Xue Yifan <xueyifan@mail.sdu.edu.cn>
2024-01-18 16:35:20 +0800

    Update selfpooldata.m

--- Underwater Vehicle/selfpooldata.m
+++ Underwater Vehicle/selfpooldata.m
@@ -10,8 +10,17 @@ switch LibraryType
         d = xaug(:,4);
         %Silvestre model
         yout = [u.*abs(u),u.*v,u.*r,v.*v,v.*r,v.*abs(v),r.*r,r.*abs(r),u.*u.*d,r.*r.*r,u.*u.*d.*d,1./u];
-        
-end
+    case 5
+        if size(xaug,1)==4   
+        xaug = xaug';
+        end
+        u = xaug(:,1);   
+        v = xaug(:,2);
+        r = xaug(:,3);
+        d = xaug(:,4);
+        %Silvestre model + Gertler model
+        yout = [v,r,d,v.*v,v.*r,v.*d,r.*r,r.*d,d.*d];        
+    end
 
 end
 
