e12f2a5e03861dfcdee6ca16e8793a97baa04d82
Xue Yifan <xueyifan@mail.sdu.edu.cn>
2024-01-18 16:42:29 +0800

    Update HLAR_starsee.m

--- Underwater Vehicle/HLAR_starsee.m
+++ Underwater Vehicle/HLAR_starsee.m
@@ -1,23 +1,20 @@
 clc,clear,close all
-%% 导入数据
+%% Import data
 load ZigZag10 ZigZag10
 load ZigZag20 ZigZag20
 load D2ZigZag25 D2ZigZag25
-%% 最优计算
-% lamda = [0.5124, 1.954];
-lamda = [0.05124, 0.1954];
+%% optimization calculation
+lamda = [0.05124, 0.1954];  %Setting Thresholds
 
-% lamda = [0.05, 0.1];
-x_train = [ZigZag10;ZigZag20];
-x_vali = D2ZigZag25;
+x_train = [ZigZag10;ZigZag20]; % training data
+x_vali = D2ZigZag25;  % test data
 x = x_train;
-x = wdenoise(x);
+x = wdenoise(x);      %denoise
 u = x(:,9)*pi/180;
 va = x(:,7);
 x = [va x(:,6) x(:,1)*pi/180]; 
 
-% dt=0.02; %In order to align the prediction process with the time in the actual data
-dt=0.2;
+dt=0.2;   %In order to align the prediction process with the time in the sensor data
 dx = zeros(length(x)-5,3);
 for i=3:length(x)-3
         for k=1:size(x,2)
@@ -43,8 +40,7 @@ for i = 1:size(Theta,2)
 end
 m = size(Theta,2);
 
-% lambda_vec = [8.2,1.4,2];   %Set sparse knob Adjusts according to Bayesian optimization results
-lambda_vec = [10,lamda2,lamda3];
+lambda_vec = [10,lamda2,lamda3];  %Set sparse knob Adjusts according to Bayesian optimization results
 
 if exist('lambda_vec') == 1
     Xi = sparsifyDynamicsIndependent(Theta,dx,lambda_vec,n-1);
@@ -56,9 +52,8 @@ for i = 1:size(Theta,2)  %Normalise the coefficient matrix
    Xi(i,:) = Xi(i,:)./Theta_norm(i);
 end
 
-yout = selfpooldatalist(Xi,LibraryType);
+yout = selfpooldatalist(Xi,LibraryType);  %display the results
 %% Make a forecast
-
 n=4;
 Nvar = 3;
 datapre = x_vali;
@@ -72,7 +67,7 @@ for k=1:size(data_pre,1)-1
 end
 
 xv = [data_pre(1,7),data_pre(1,6),data_pre(1,1)*pi/180];
-psi_pre = data_pre(1,10);  %存储艏向角信息
+psi_pre = data_pre(1,10);  %Storing bow angle information
 x_p(1,:)=xv;
 u_p = data_pre(:,9)*pi/180; 
 
@@ -80,13 +75,13 @@ for k=1:size(data_pre,1)-1   %Prediction using Eulerian dispersion methods
     y=[x_p(k,:) u_p(k)];
     xPool = selfpooldata(y,LibraryType);
     dxPool = xPool*Xi(:,1:Nvar);
-    x_p(k+1,:) = x_p(k,:)+(dt*dxPool) ;      %Update next status
+    x_p(k+1,:) = x_p(k,:)+(dt*dxPool) ;             %Update next status
     psi_pre(k+1) = psi_pre(k)+x_p(k+1,3)*dtt*180/pi;
 end
 
 x_p(:,3) = x_p(:,3)*180/pi;
 
-%% 绘制预测验证图-----------------------------------------------------------
+%% Plot-----------------------------------------------------------
 load r_KT r_KT
 load psi_KT psi_KT
 
@@ -129,8 +124,7 @@ legend([h1,h2,h3,h4],'Proposed HLAR method','Experiment','semi- K-T equation','\
 set(gcf, 'Position', [100, 100, 800, 600]); % 设置Figure窗口的位置和大小
 % print('2KT and sindy graph','-r600','-dpng')
 
-%% --------------------------------------------------------------------------
-%计算均方根误差
-rmse_v = sqrt(mean((x_p(:,2)-data_pre(:,6)).^2))
+%% Plot--------------------------------------------------------------------------
+rmse_v = sqrt(mean((x_p(:,2)-data_pre(:,6)).^2))   %Calculate the root mean square error
 rmse_r = sqrt(mean((x_p(:,3)-data_pre(:,1)).^2))*pi/180
 rmse_psi = sqrt(mean((psi_pre'-data_pre(:,10)).^2))*pi/180
